535192ef85288e9a8e12982ffb798bc36eabc154
juanjohiguita <95238272+juanjohiguita@users.noreply.github.com>
2023-04-23 22:51:50 -0500

    Error post en para las competencias
    Hay un error cuando se intenta hacer post en competencias, los demas : formatos, usuarios y votaciones si dejan hacer post. Error unico de competencias

--- src/registro-user.js
+++ src/registro-user.js
@@ -43,7 +43,7 @@ const RegistroUser = () => {
       console.log("Entre al perreo")
       try {
         let result = await fetch(
-          'http://localhost:5000/api/usuarios', {
+          'http://localhost:5000/api/formatos', {
               method: "post",
               body: JSON.stringify({ nombre, apellido, email, password, aka, roles }),
               headers: {
@@ -61,7 +61,7 @@ const RegistroUser = () => {
       console.log("Entre al perreo")
       try {
         let result = await fetch(
-          'http://localhost:5000/api/usuarios', {
+          'http://localhost:5000/api/competencias', {
               method: "get",
               headers: {
                   'Content-Type': 'application/json'
@@ -124,7 +124,7 @@ const RegistroUser = () => {
           style={{height:25}}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <MDBBtn onClick={(e) => testeoLoginGet(e)} rounded color='success' size='lg'>Registrar</MDBBtn>
+        <MDBBtn onClick={(e) => testeoLoginPost(e)} rounded color='success' size='lg'>Registrar</MDBBtn>
         <div className='ingreso-correo'>
           <span>-------- O Registrarse con --------</span>
           <br/>
--- src/registroTorneo.js
+++ src/registroTorneo.js
@@ -1,31 +1,50 @@
-import { MDBInput } from 'mdb-react-ui-kit'
-import React, { useState } from 'react'
+import { MDBBtn, MDBIcon, MDBInput } from 'mdb-react-ui-kit';
+import React from 'react';
+import { useState } from 'react';
 
 const RegistroTorneo = () => {
   
-  const [nombre_competencia, setNombre_Competencia] = useState('')
-  const [tipo_competencia, setTipo_Competencia] = useState('')
-  const [descripcion, setDescripcion] = useState('')
-  const [numero_fechas, setNumero_Fechas] = useState('')
-  const [jueces, setJueces] = useState([]) 
+  const [nombre_competencia, setNombreCompetencia] = useState("");
+  const [tipo_competencia, setTipo_Competencia] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [numero_fechas, setNumeroFechas] = useState("");
 
-  const TorneoPost = async (e) =>{
-    console.log("Entre al perreo")
-    try {
-      let result = await fetch(
-        'http://localhost:5000/api/competencias', {
-            method: "post",
-            body: JSON.stringify({ nombre_competencia, descripcion, numero_fechas, jueces}),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        result = await result.json();
-        console.warn(result);
-    } catch (error) {
-      console.log(error);
+
+    const TorneoPost = async (e) =>{
+      console.log("Entre al perreo")
+      try {
+        let result = await fetch(
+          'http://localhost:5000/api/competencias', {
+              method: "post",
+              body: JSON.stringify({descripcion}),
+              headers: {
+                  'Content-Type': 'application/json'
+              }
+          })
+          console.log(result)
+          result = await result.json();
+          console.warn(result);
+      } catch (error) {
+        console.log(error);
+      }
     }
-  }
+
+    const  TorneoGet = async (e) =>{
+      console.log("Entre al perreo")
+      try {
+        let result = await fetch(
+          'http://localhost:5000/api/competencias', {
+              method: "get",
+              headers: {
+                  'Content-Type': 'application/json'
+              }
+          })
+          result = await result.json();
+          console.warn(result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
 
   
   return (
@@ -45,7 +64,10 @@ const RegistroTorneo = () => {
       </div>
       <br/>
       <div className='nombre-torneo'>
-          <MDBInput wrapperClass='mb-4' style={{height:25}} label='Nombre del torneo'></MDBInput>
+          <MDBInput wrapperClass='mb-4' style={{height:25}} label='Nombre del torneo'
+          id="nombre_competencia"
+          onChange={(e) => setNombreCompetencia(e.target.value)}
+          ></MDBInput>
           <select>
               <option>Liga</option>
               <option>Copa</option>
@@ -55,24 +77,30 @@ const RegistroTorneo = () => {
           <MDBInput wrapperClass='mb-4'
             style={{height:25}} 
             size='lg'
+            id="numero_mcs"
             label='Número de MCs'
-            onChange={(e) => setNombre_Competencia(e.target.value)}>
+            >
           </MDBInput>
           <MDBInput wrapperClass='mb-4' 
             style={{height:25}} 
             label='Número de Fechas'
-            onChange={(e) => setNumero_Fechas(e.target.value)}>
+            id="numero_fechas"
+            onChange={(e) => setNumeroFechas(e.target.value)}>
           </MDBInput>
           <MDBInput wrapperClass='mb-4' 
-          style={{height:25}} 
-          label='Número de Jueces'>
+            tyle={{height:25}} 
+            label='Número de Jueces'
+            id="numero_jueces"
+            >
+            
           </MDBInput>
           <MDBInput wrapperClass='mb-4' 
-          style={{height:45}} 
-          label='Descripción del Torneo'
-          onChange={(e) => setDescripcion(e.target.value)}></MDBInput>
-      </div>
-      <button className='boton-registrar' onClick={(e) => TorneoPost(e)}>Registrar</button>    
+            style={{height:45}} 
+            id="descripcion"
+            label='Descripción del Torneo'
+            onChange={(e) => setDescripcion(e.target.value)}></MDBInput>
+      </div>  
+      <MDBBtn onClick={(e) => TorneoPost(e)} rounded color='success' size='lg'>Registrar</MDBBtn>
     </div>
   )
 }
