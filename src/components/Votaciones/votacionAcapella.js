22abd689561e17dbd84acf647dd8ed744517a84f
Jovan Alejandro Zambrano Bello <75374943+a-zambrano1@users.noreply.github.com>
2023-05-08 09:14:32 -0500

    Actualización módulo de formato Acapella

--- src/components/Votaciones/votacionAcapella.js
+++ src/components/Votaciones/votacionAcapella.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import Entrada4x4 from '../../utils/entradas'
+import { MDBBtn, MDBInput } from 'mdb-react-ui-kit'
+import { useState } from 'react'
 
-const VotacionAcapella = () => {
+const VotacionAcapella = ({mc1,mc2}) => {
+  
+  const [numEntradas,setNumEntradas] = useState(0)
+  const [entradas, setEntradas] = useState(0)
+
+  const renderEntrada = () => {
+      setEntradas(numEntradas);
+    }
+  
   return (
-    <div>VotacionAcapella</div>
+    <div>
+      <div className='panel-votacion'>
+          <div>
+            <MDBInput label='Ingrese número de entradas' type='number' onChange={(e) => setNumEntradas(e.target.value)}></MDBInput>
+            <MDBBtn class='btn btn-success' onClick={renderEntrada}>Iniciar</MDBBtn>
+          </div>
+          <div className='titulo-mcs'>
+            <span>{mc1}</span>
+            <span>{mc2}</span>
+          </div>
+          <div className='entradas-votacion'>
+            <div className='separacion-entradas'>
+              {Array.from({length: entradas}, (_,i) => <Entrada4x4 key={i} />)}
+            </div>
+            <div className='separacion-entradas'>
+              {Array.from({length: entradas}, (_,i) => <Entrada4x4 key={i} />)}
+            </div>
+          </div>
+        </div>
+    </div>
   )
 }
 
