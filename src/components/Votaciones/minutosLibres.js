2a664a3ad3533f239f52413d70755395504de53f
Jovan Alejandro Zambrano Bello <jovan.zambrano@udea.edu.co>
2023-05-16 22:37:48 -0500

    update a minutosLibre.js

--- src/components/Votaciones/minutosLibres.js
+++ src/components/Votaciones/minutosLibres.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Entrada4x4 from '../../utils/entradas'
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react'
 
-const MinutosLibres = ({mc1, mc2, onSaveEntradas, numEnt = 6, listValuesMC1, listValuesMC2}) => {
+const MinutosLibres = ({mc1, mc2, onSaveEntradas, numEnt = 6,onSaveNEntr, listValuesMC1, listValuesMC2}) => {
   
   const location = useLocation();
+
+  const [entradas, setEntradas] = useState(numEnt)
+
+  const renderEntrada = () => {
+    onSaveNEntr(entradas)
+  }
 
   const handleChangeEntradas = (idMC, index, value) => {
    onSaveEntradas(idMC, index, value)
@@ -13,6 +20,7 @@ const MinutosLibres = ({mc1, mc2, onSaveEntradas, numEnt = 6, listValuesMC1, lis
   
   return (
     <div>
+        {renderEntrada()}
         <div className='titulo-mcs'>
             <span>{mc1}</span>
             <span>{mc2}</span>
