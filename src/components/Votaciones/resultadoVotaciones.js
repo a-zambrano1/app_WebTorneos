2e561e4d357f2ceb28ba2ee6497fb4f5323d348b
Jovan Alejandro Zambrano Bello <jovan.zambrano@udea.edu.co>
2023-05-29 17:59:21 -0500

    Agregacion de toastify al componente de resultados

--- src/components/Votaciones/resultadoVotaciones.js
+++ src/components/Votaciones/resultadoVotaciones.js
@@ -1,7 +1,17 @@
 import { MDBBtn } from 'mdb-react-ui-kit'
 import React from 'react'
+import { toast } from 'react-toastify'
 
 const ResultadoVotaciones = ({resultado1, resultado2}) => {
+  
+  const guardar = () => {
+    if(resultado1 === 0 && resultado2 === 0){
+      toast.success('Guardado con exito')
+    }else{
+      toast.error('Error al guardar')
+    }
+  }
+  
   return (
     <div>
       <span className='titulo-resultados'>RESULTADOS</span>
@@ -9,7 +19,7 @@ const ResultadoVotaciones = ({resultado1, resultado2}) => {
         <span>{resultado1}</span>
         <span>{resultado2}</span>
       </div>
-      <MDBBtn className='btn btn-success'>Guardar</MDBBtn>
+      <MDBBtn onClick={guardar} className='btn btn-success'>Guardar</MDBBtn>
     </div>
   )
 }
